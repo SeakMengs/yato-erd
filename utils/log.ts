80e9feaef651bc0a29788238d2dd2b79f16682bf
SeakMengs <hseakmeng22@gmail.com>
2024-09-20 12:59:32 +0700

    Fix pino pretty on production

--- utils/log.ts
+++ utils/log.ts
@@ -1,11 +1,12 @@
 import pino from "pino";
+import "pino-pretty";
 
 export const logger = pino({
   transport: {
     target: "pino-pretty",
     options: {
       translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-      ignore: "pid.hostname",
+      ignore: "pid,hostname",
     },
   },
   browser: {
