a751a1300f341a2d2a6799de13250d885e768fef
SeakMengs <hseakmeng22@gmail.com>
2024-09-20 13:03:18 +0700

    Remove pino pretty

--- utils/log.ts
+++ utils/log.ts
@@ -1,14 +1,14 @@
 import pino from "pino";
-import "pino-pretty";
+// import "pino-pretty";
 
 export const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-      ignore: "pid,hostname",
-    },
-  },
+  // transport: {
+  //   target: "pino-pretty",
+  //   options: {
+  //     translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+  //     ignore: "pid,hostname",
+  //   },
+  // },
   browser: {
     // asObject: true,
     disabled: !import.meta.dev,
